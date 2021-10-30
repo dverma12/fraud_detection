1498b0ade5fdd379b0208dee5d7c5dea52ba91b0
dverma12 <deepakverma1693@gmail.com>
2021-10-30 17:47:20 +0530

    model3 is changed

--- ml_model.py
+++ ml_model.py
@@ -9,7 +9,7 @@ def model2():
     print("Adding code for ML Model2")
 
 def model3():
-    print("Adding code for ML Model3")
+    print("Adding code for ML Model")
 
 
 print("model2 performs the best")
