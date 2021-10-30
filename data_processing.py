4a567b6d922e092c5da7aa5d880be7ff706b5546
dverma12 <deepakverma1693@gmail.com>
2021-10-30 16:05:17 +0530

    Revert "Revert "adding a new fuction sum""
    This reverts commit c16da5ccbf065d6ecbc01e7f7615b37bc4ba0072.

---
+++ data_processing.py
@@ -0,0 +1,15 @@
+
+print("adding  all the data processing related to fraud detection ")
+
+
+a=5
+b=7
+
+
+def sum(a,b):
+    c=a+b
+    return a
+
+
+print("added a new function sum which is not correct")
+
